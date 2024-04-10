1f4589ec905d9b84118d5d8d3ed9e67d3aebc129
Manuel <manurz2121@gmail.com>
2024-04-10 15:30:12 -0300

    green: TestCanRegisterWithValidCredentials passed - Created CredentialsManager class - Implemented Register and IsRegistered methods

--- BusinessLogic/CredentialsManager.cs
+++ BusinessLogic/CredentialsManager.cs
@@ -2,4 +2,32 @@
 
 public class CredentialsManager
 {
+    private List<Credentials> CredentialsList { set; get; }
+
+    public CredentialsManager()
+    {
+        CredentialsList = new List<Credentials>();
+    }
+
+    public bool IsRegistered(string email)
+    {
+        return CredentialsList.Any(c => c.Email == email);
+    }
+
+    public void Register(string email, string password, string passwordConfirmation)
+    {
+        CredentialsList.Add(new Credentials(email, password));
+    }
+}
+
+public readonly struct Credentials
+{
+    public string Email { init; get; }
+    public string Password { init; get; }
+
+    public Credentials(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
 }
