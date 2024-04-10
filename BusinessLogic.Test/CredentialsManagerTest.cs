be07b6856d7f664e0c89d3e0c34f313c62e2eb0e
Manuel <manurz2121@gmail.com>
2024-04-10 15:35:29 -0300

    red: Add TestCanLoginWithValidCredentials

--- BusinessLogic.Test/CredentialsManagerTest.cs
+++ BusinessLogic.Test/CredentialsManagerTest.cs
@@ -15,4 +15,18 @@ public class CredentialsManagerTest
         // Assert
         Assert.IsTrue(credsManager.IsRegistered("test@test.com"));
     }
+    
+    [TestMethod]
+    public void TestCanLoginWithValidCredentials()
+    {
+        // Arrange
+        var credsManager = new CredentialsManager();
+        
+        // Act
+        credsManager.Register("test@test.com", "12345678@mE", "12345678@mE");
+        var credentials = credsManager.Login("test@test.com", "12345678@mE");
+        
+        // Assert
+        Assert.AreSame(credentials.Email, "test@test.com");
+    }
 }
