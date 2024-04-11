e503b8ac982c9d0cd006e2d6ef8cb410b8fb25b0
Manuel <manurz2121@gmail.com>
2024-04-11 16:18:37 -0300

    green: TestCantCreateUserWithInvalidEmailFormat passed

--- BusinessLogic/User.cs
+++ BusinessLogic/User.cs
@@ -1,9 +1,25 @@
+using System.Text.RegularExpressions;
+
 namespace BusinessLogic;
 
 public class User
 {
     public string Name { get; }
-    public string Email { get; }
+    private string _email;
+    public string Email
+    {
+        get => _email;
+        set
+        {
+            if (!Regex.IsMatch(value, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                throw new ArgumentException("Email format is invalid.");
+            }
+
+            _email = value;
+        }
+    }
+
     public string Password { get; }
 
     public User(string name, string email, string password)
