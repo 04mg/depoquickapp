ab203eb5fbc42e4694fb9fbb92a29f285452bf19
Manuel <manurz2121@gmail.com>
2024-04-11 16:10:11 -0300

    green: TestCanCreateUserWithValidData passed - Created user class with properties and constructor

---
+++ BusinessLogic/User.cs
@@ -0,0 +1,15 @@
+namespace BusinessLogic;
+
+public class User
+{
+    public string Name { get; }
+    public string Email { get; }
+    public string Password { get; }
+
+    public User(string name, string email, string password)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+    }
+}
