5da625c9c747d3a9ef471b52bc0a8ec632a3bb58
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-04-18 13:27:24 -0300

    red: Add TestCantCreatePromotionWithLengthGreaterThan20

--- BusinessLogic.Test/PromotionTest.cs
+++ BusinessLogic.Test/PromotionTest.cs
@@ -30,4 +30,19 @@ public class PromotionTest
         // Assert
         Assert.AreEqual("Label format is invalid, it can't contain symbols", exception.Message);
     }
+
+    [TestMethod]
+    public void TestCantCreatePromotionWithLengthGreaterThan20()
+    {
+        //Arrange
+        var from = DateOnly.FromDateTime(DateTime.Now);
+        var to = DateOnly.FromDateTime(DateTime.Now.AddDays(1));
+        var invalidLabel = new string('a', 21);
+        
+        //Act
+        var exception = Assert.ThrowsException<ArgumentException>(() => new Promotion(invalidLabel, 50, from, to));
+        
+        //Assert
+        Assert.AreEqual("Label format is invalid, length must be lesser or equal than 20", exception.Message);
+    }
 }
