a60bde312e5661410d5dc8046462eba6f39ebd86
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-04-21 14:39:16 -0300

    green: TestCanDeletePromotion passed

--- BusinessLogic/PromotionManager.cs
+++ BusinessLogic/PromotionManager.cs
@@ -3,12 +3,12 @@ namespace BusinessLogic;
 public class PromotionManager
 {
     public List<Promotion> Promotions { get; private set; }
-    
+
     public PromotionManager()
     {
         Promotions = new List<Promotion>();
     }
-    
+
     public void Add(PromotionModel promotionModel)
     {
         Promotions.Add(new Promotion(promotionModel));
@@ -16,6 +16,16 @@ public class PromotionManager
 
     public void Delete(PromotionModel model)
     {
-        throw new NotImplementedException();
+        foreach (var promotion in Promotions)
+        {
+            var dateFrom = DateOnly.FromDateTime(DateTime.Parse(model.DateFrom));
+            var dateTo = DateOnly.FromDateTime(DateTime.Parse(model.DateTo));
+            if (promotion.Label == model.Label && promotion.Discount == model.Discount &&
+                promotion.Validity.Item1 == dateFrom && promotion.Validity.Item2 == dateTo)
+            {
+                Promotions.Remove(promotion);
+                return;
+            }
+        }
     }
 }
