f5ab026220fccb9de07b78c2d37d0deb6f9bb31c
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-04-21 14:42:49 -0300

    green: TestCanModifyPromotion passed

--- BusinessLogic/PromotionManager.cs
+++ BusinessLogic/PromotionManager.cs
@@ -31,6 +31,20 @@ public class PromotionManager
 
     public void Modify(PromotionModel model, PromotionModel newModel)
     {
-        throw new NotImplementedException();
+        foreach (var promotion in Promotions)
+        {
+            var dateFrom = DateOnly.FromDateTime(DateTime.Parse(model.DateFrom));
+            var dateTo = DateOnly.FromDateTime(DateTime.Parse(model.DateTo));
+            if (promotion.Label == model.Label && promotion.Discount == model.Discount &&
+                promotion.Validity.Item1 == dateFrom && promotion.Validity.Item2 == dateTo)
+            {
+                promotion.Label = newModel.Label;
+                promotion.Discount = newModel.Discount;
+                promotion.Validity = new Tuple<DateOnly, DateOnly>(
+                    DateOnly.FromDateTime(DateTime.Parse(newModel.DateFrom)),
+                    DateOnly.FromDateTime(DateTime.Parse(newModel.DateTo)));
+                return;
+            }
+        }
     }
 }
