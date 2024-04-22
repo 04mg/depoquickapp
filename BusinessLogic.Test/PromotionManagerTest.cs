15dd26b4e56c4f2b8527b5571432e9bdaac09440
Manuel <manurz2121@gmail.com>
2024-04-22 15:04:32 -0300

    refactor: Handle credentials in TestInitialize

--- BusinessLogic.Test/PromotionManagerTest.cs
+++ BusinessLogic.Test/PromotionManagerTest.cs
@@ -7,12 +7,42 @@ public class PromotionManagerTest
     private const int Discount = 50;
     private readonly DateOnly _today = DateOnly.FromDateTime(DateTime.Now);
     private readonly DateOnly _tomorrow = DateOnly.FromDateTime(DateTime.Now.AddDays(1));
+    private PromotionManager _promotionManager = new();
+    private Credentials _adminCredentials;
+    private Credentials _clientCredentials;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _promotionManager = new PromotionManager();
+        var authManager = new AuthManager();
+
+        var adminModel = new UserModel()
+        {
+            Email = "admin@admin.com",
+            Password = "12345678@mE",
+            NameSurname = "Name Surname",
+            Rank = UserRank.Administrator
+        };
+
+        var clientModel = new UserModel()
+        {
+            Email = "client@client.com",
+            Password = "12345678@mE",
+            NameSurname = "Name Surname",
+            Rank = UserRank.Client
+        };
+
+        authManager.Register(adminModel, adminModel.Password);
+        authManager.Register(clientModel, clientModel.Password);
+        _adminCredentials = authManager.Login(adminModel.Email, adminModel.Password);
+        _clientCredentials = authManager.Login(clientModel.Email, clientModel.Password);
+    }
 
     [TestMethod]
     public void TestCanAddPromotion()
     {
         // Arrange
-        var promotionManager = new PromotionManager();
         var addDto = new AddPromotionDto()
         {
             Label = Label,
@@ -20,29 +50,18 @@ public class PromotionManagerTest
             DateFrom = _today,
             DateTo = _tomorrow
         };
-        var authManager = new AuthManager();
-        var adminModel = new UserModel()
-        {
-            Email = "test@test.com",
-            Password = "12345678@mE",
-            NameSurname = "Name Surname",
-            Rank = UserRank.Administrator
-        };
-        authManager.Register(adminModel, adminModel.Password);
-        var credentials = authManager.Login(adminModel.Email, adminModel.Password);
 
         // Act
-        promotionManager.Add(addDto, credentials);
+        _promotionManager.Add(addDto, _adminCredentials);
 
         // Assert
-        Assert.AreEqual(1, promotionManager.Promotions.Count);
+        Assert.AreEqual(1, _promotionManager.Promotions.Count);
     }
 
     [TestMethod]
     public void TestCanDeletePromotion()
     {
         // Arrange
-        var promotionManager = new PromotionManager();
         var addDto = new AddPromotionDto()
         {
             Label = Label,
@@ -51,30 +70,19 @@ public class PromotionManagerTest
             DateTo = _tomorrow
         };
         var promotion = new Promotion(1, addDto.Label, addDto.Discount, addDto.DateFrom, addDto.DateTo);
-        var authManager = new AuthManager();
-        var adminModel = new UserModel()
-        {
-            Email = "test@test.com",
-            Password = "12345678@mE",
-            NameSurname = "Name Surname",
-            Rank = UserRank.Administrator
-        };
-        authManager.Register(adminModel, adminModel.Password);
-        var credentials = authManager.Login(adminModel.Email, adminModel.Password);
-        promotionManager.Add(addDto, credentials);
+        _promotionManager.Add(addDto, _adminCredentials);
 
         // Act
-        promotionManager.Delete(1);
+        _promotionManager.Delete(1);
 
         // Assert
-        Assert.IsFalse(promotionManager.Promotions.Contains(promotion));
+        Assert.IsFalse(_promotionManager.Promotions.Contains(promotion));
     }
 
     [TestMethod]
     public void TestCanModifyPromotion()
     {
         // Arrange
-        var promotionManager = new PromotionManager();
         var addDto = new AddPromotionDto()
         {
             Label = Label,
@@ -82,18 +90,10 @@ public class PromotionManagerTest
             DateFrom = _today,
             DateTo = _tomorrow
         };
+
         var promotion = new Promotion(1, addDto.Label, addDto.Discount, addDto.DateFrom, addDto.DateTo);
-        var authManager = new AuthManager();
-        var adminModel = new UserModel()
-        {
-            Email = "test@test.com",
-            Password = "12345678@mE",
-            NameSurname = "Name Surname",
-            Rank = UserRank.Administrator
-        };
-        authManager.Register(adminModel, adminModel.Password);
-        var credentials = authManager.Login(adminModel.Email, adminModel.Password);
-        promotionManager.Add(addDto, credentials);
+        _promotionManager.Add(addDto, _adminCredentials);
+
         var modifyDto = new ModifyPromotionDto()
         {
             Id = 1,
@@ -104,37 +104,28 @@ public class PromotionManagerTest
         };
 
         // Act
-        promotionManager.Modify(modifyDto);
+        _promotionManager.Modify(modifyDto);
 
         // Assert
-        Assert.IsFalse(promotionManager.Promotions.Contains(promotion));
+        Assert.IsFalse(_promotionManager.Promotions.Contains(promotion));
     }
 
     [TestMethod]
     public void TestOnlyAdministratorsCanAddPromotion()
     {
         // Arrange
-        var promotionManager = new PromotionManager();
-        var authManager = new AuthManager();
-        var model = new AddPromotionDto()
+        var addDto = new AddPromotionDto()
         {
             Label = Label,
             Discount = Discount,
             DateFrom = _today,
             DateTo = _tomorrow
         };
-        var clientModel = new UserModel()
-        {
-            Email = "test@test.com",
-            Password = "12345678@mE",
-            NameSurname = "Name Surname",
-            Rank = UserRank.Client
-        };
-        authManager.Register(clientModel, clientModel.Password);
-        var credentials = authManager.Login(clientModel.Email, clientModel.Password);
 
         // Act
-        var exception = Assert.ThrowsException<UnauthorizedAccessException>(() => promotionManager.Add(model, credentials));
+        var exception =
+            Assert.ThrowsException<UnauthorizedAccessException>(() =>
+                _promotionManager.Add(addDto, _clientCredentials));
 
         // Assert
         Assert.AreEqual("Only administrators can add promotions.", exception.Message);
