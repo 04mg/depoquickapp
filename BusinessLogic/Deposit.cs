a3a803d0388e3f7b1478a3f2802c85c467343988
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-04-23 15:48:12 -0300

    green: TestCantCreateDepositWithInvalidArea passed

--- BusinessLogic/Deposit.cs
+++ BusinessLogic/Deposit.cs
@@ -6,11 +6,33 @@ public class Deposit
     private string _small;
     private bool _climateControl;
     private List<AddPromotionDto> _promotionList;
+
+    public string Area
+    {
+        get => _area;
+        set
+        {
+            if(value != "A" && value != "B" && value != "C" && value != "D" && value != "E")
+            {
+                throw new ArgumentException("Invalid area.");
+            }
+            _area = value;
+        }
+    }
     public Deposit(string area, string small, bool climateControl, List<AddPromotionDto> promotionList)
     {
-        _area = area;
+        Area = area;
         _small = small;
         _climateControl = climateControl;
         _promotionList = promotionList;
     }
+}
+
+public enum Area
+{
+    A,
+    B,
+    C,
+    D,
+    E
 }
