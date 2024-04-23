9cbee1e8b0ef17ca16cd462972a2949ae153b173
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-04-23 15:42:37 -0300

    green: TestCanCreateDepositWithValidData passed

--- BusinessLogic/Deposit.cs
+++ BusinessLogic/Deposit.cs
@@ -2,8 +2,15 @@ namespace BusinessLogic;
 
 public class Deposit
 {
+    private string _area;
+    private string _small;
+    private bool _climateControl;
+    private List<AddPromotionDto> _promotionList;
     public Deposit(string area, string small, bool climateControl, List<AddPromotionDto> promotionList)
     {
-        throw new NotImplementedException();
+        _area = area;
+        _small = small;
+        _climateControl = climateControl;
+        _promotionList = promotionList;
     }
 }
