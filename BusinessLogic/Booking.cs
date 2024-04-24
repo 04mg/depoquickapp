0986687dff93663d8950ca4625c29f730b54d455
Manuel <manurz2121@gmail.com>
2024-04-24 15:41:03 -0300

    green: TestCantCreateBookingWithDateFromGreaterThanDateTo passed

--- BusinessLogic/Booking.cs
+++ BusinessLogic/Booking.cs
@@ -2,10 +2,28 @@ namespace BusinessLogic;
 
 public class Booking
 {
+    private Tuple<DateOnly, DateOnly> _duration = new(new DateOnly(), new DateOnly());
     public int Id { get; }
     public int DepositId { get; }
     public string Email { get; }
-    public Tuple<DateOnly, DateOnly> Duration { get; set; }
+
+    public Tuple<DateOnly, DateOnly> Duration
+    {
+        get => _duration;
+        set
+        {
+            EnsureDateFromIsLesserThanDateTo(value.Item1, value.Item2);
+            _duration = value;
+        }
+    }
+
+    private static void EnsureDateFromIsLesserThanDateTo(DateOnly dateFrom, DateOnly dateTo)
+    {
+        if (dateFrom > dateTo)
+        {
+            throw new ArgumentException("The starting date of the booking must not be later than the ending date.");
+        }
+    }
 
     public Booking(int id, int depositId, string email, DateOnly startDate, DateOnly endDate)
     {
