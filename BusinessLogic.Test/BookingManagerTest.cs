9aaf2323f7c7aa88ee8df2e11973be11d8ce5315
Enzo Izquierdo <enzo.izquierdo.d@gmail.com>
2024-04-24 17:04:27 -0300

    red: Add TestCanAddBooking

---
+++ BusinessLogic.Test/BookingManagerTest.cs
@@ -0,0 +1,76 @@
+namespace BusinessLogic.Test;
+
+[TestClass]
+public class BookingManagerTest
+{
+
+    private AuthManager _authManager = new();
+    private DepositManager _depositManager = new();
+    private Credentials _credentials = new();
+    private BookingManager bookingManager = new();
+    
+    [TestInitialize]
+    public void Initialize()
+    {
+        _authManager = new AuthManager();
+        var adminModel = new RegisterDto()
+        {
+            NameSurname = "Name Surname",
+            Email = "admin@test.com",
+            Password = "12345678@mE",
+            PasswordConfirmation = "12345678@mE",
+            Rank = "Administrator"
+        };
+        _authManager.Register(adminModel);
+        _credentials = _authManager.Login(new LoginDto() { Email = "admin@test.com", Password = "12345678@mE" });
+
+        var userModel = new RegisterDto()
+        {
+            NameSurname = "Name Surname",
+            Email = "test@test.com",
+            Password = "12345678@mE",
+            PasswordConfirmation = "12345678@mE",
+            Rank = "Client"
+        };
+        _authManager.Register(userModel);
+
+        var promotionManager = new PromotionManager();
+        var promotionModel1 = new AddPromotionDto()
+        {
+            Label = "label",
+            Discount = 50,
+            DateFrom = DateOnly.FromDateTime(DateTime.Now),
+            DateTo = DateOnly.FromDateTime(DateTime.Now.AddDays(1))
+        };
+        promotionManager.Add(promotionModel1, _credentials);
+        var promotionList = new List<int>() { 1 };
+
+        var depositModel = new AddDepositDto()
+        {
+            Area = "A",
+            Size = "Small",
+            ClimateControl = true,
+            PromotionList = promotionList
+        };
+        _depositManager.Add(depositModel, _credentials, promotionManager);
+    }
+
+    [TestMethod]
+    public void TestCanAddBooking()
+    {
+        // Arrange
+        var addBookingDto = new AddBookingDto()
+        {
+            DepositId = 1,
+            Email = "test@test.com",
+            DateFrom = DateOnly.FromDateTime(DateTime.Now),
+            DateTo = DateOnly.FromDateTime(DateTime.Now.AddDays(1))
+        };
+
+        // Act
+        bookingManager.Add(addBookingDto, _depositManager, _authManager);
+        
+        // Assert
+        Assert.AreEqual(1, bookingManager.Bookings.Count);
+    }
+}
---
+++ BusinessLogic/BookingManager.cs
@@ -0,0 +1,16 @@
+namespace BusinessLogic;
+
+public class BookingManager
+{
+    public List<Booking> Bookings { get; set; }
+    
+    public BookingManager()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(AddBookingDto addBookingDto, DepositManager depositManager, AuthManager authManager)
+    {
+        throw new NotImplementedException();
+    }
+}
