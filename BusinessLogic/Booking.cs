58c8f1aee3fe1dbd0edcc1ef5c31d0e1b19a90c4
Manuel <manurz2121@gmail.com>
2024-04-24 15:26:45 -0300

    green: TestCanCreateBookingWithValidData passed

--- BusinessLogic/Booking.cs
+++ BusinessLogic/Booking.cs
@@ -2,5 +2,16 @@ namespace BusinessLogic;
 
 public class Booking
 {
-    
+    public int Id { get; }
+    public int DepositId { get; }
+    public string Email { get; }
+    public Tuple<DateOnly, DateOnly> Duration { get; set; }
+
+    public Booking(int id, int depositId, string email, DateOnly startDate, DateOnly endDate)
+    {
+        Id = id;
+        DepositId = depositId;
+        Email = email;
+        Duration = new Tuple<DateOnly, DateOnly>(startDate, endDate);
+    }
 }
