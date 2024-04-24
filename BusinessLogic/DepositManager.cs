0b7336b72bf6a2625904a55e18aa28911bee7835
Manuel <manurz2121@gmail.com>
2024-04-24 09:35:59 -0300

    green: TestCanDeleteDeposit passed

--- BusinessLogic/DepositManager.cs
+++ BusinessLogic/DepositManager.cs
@@ -15,4 +15,17 @@ public class DepositManager
             addDepositDto.PromotionList, promotionManager);
         Deposits.Add(deposit);
     }
+
+    public void Delete(AddDepositDto addDepositDto, Credentials credentials)
+    {
+        var deposit = Deposits.FirstOrDefault(d =>
+            d.Area == addDepositDto.Area && d.Size == addDepositDto.Size &&
+            d.ClimateControl == addDepositDto.ClimateControl);
+        if (deposit == null)
+        {
+            return;
+        }
+
+        Deposits.Remove(deposit);
+    }
 }
