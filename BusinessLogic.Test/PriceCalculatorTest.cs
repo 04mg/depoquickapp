63e80ee9e11b47f02bc24fbed0825d5effa0db21
Manuel <manurz2121@gmail.com>
2024-04-28 20:43:08 -0300

    red: TestCanCalculatePriceWithMultiplePromotionsUnderFullDiscount

--- BusinessLogic.Test/PriceCalculatorTest.cs
+++ BusinessLogic.Test/PriceCalculatorTest.cs
@@ -21,7 +21,7 @@ public class PriceCalculatorTest
             new Tuple<DateOnly, DateOnly>(_today, _today.AddDays(1)));
         Assert.AreEqual(expectedPrice, price);
     }
-    
+
     [TestMethod]
     [DataRow("Small", false, 7, 332.5)]
     [DataRow("Small", false, 15, 675)]
@@ -44,7 +44,7 @@ public class PriceCalculatorTest
             new Tuple<DateOnly, DateOnly>(_today, _today.AddDays(duration)));
         Assert.AreEqual(expectedPrice, price);
     }
-    
+
     [TestMethod]
     [DataRow(5, "Small", false, 1, 47.5)]
     [DataRow(5, "Small", false, 7, 315)]
@@ -70,6 +70,38 @@ public class PriceCalculatorTest
     {
         var promotion = new Promotion(1, "label", discount, _today, _today.AddDays(durationInDays));
         var promotions = new List<Promotion> { promotion };
+        var deposit = new Deposit(1, "A", size, climateControl, promotions);
+        var priceCalculator = new PriceCalculator();
+        var price = priceCalculator.CalculatePrice(deposit,
+            new Tuple<DateOnly, DateOnly>(_today, _today.AddDays(durationInDays)));
+        Assert.AreEqual(expectedPrice, price);
+    }
+
+    [TestMethod]
+    [DataRow(5, 5, "Small", false, 1, 45)]
+    [DataRow(10, 15, "Small", false, 7, 245)]
+    [DataRow(5, 10, "Small", false, 15, 562.5)]
+    [DataRow(5, 5, "Medium", false, 1, 67.5)]
+    [DataRow(10, 15, "Medium", false, 7, 367.5)]
+    [DataRow(5, 10, "Medium", false, 15, 843.75)]
+    [DataRow(5, 5, "Large", false, 1, 90)]
+    [DataRow(10, 15, "Large", false, 7, 490)]
+    [DataRow(5, 10, "Large", false, 15, 1125)]
+    [DataRow(5, 5, "Small", true, 1, 63)]
+    [DataRow(10, 15, "Small", true, 7, 343)]
+    [DataRow(5, 10, "Small", true, 15, 787.5)]
+    [DataRow(5, 5, "Medium", true, 1, 85.5)]
+    [DataRow(10, 15, "Medium", true, 7, 465.5)]
+    [DataRow(5, 10, "Medium", true, 15, 1068.75)]
+    [DataRow(5, 5, "Large", true, 1, 108)]
+    [DataRow(10, 15, "Large", true, 7, 588)]
+    [DataRow(5, 10, "Large", true, 15, 1350)]
+    public void TestCanCalculatePriceWithMultiplePromotionsUnderFullDiscount(int discount1, int discount2, string size,
+        bool climateControl, int durationInDays, double expectedPrice)
+    {
+        var promotion1 = new Promotion(1, "label", discount1, _today, _today.AddDays(durationInDays));
+        var promotion2 = new Promotion(2, "label", discount2, _today, _today.AddDays(durationInDays));
+        var promotions = new List<Promotion> { promotion1, promotion2 };
         var deposit = new Deposit(1, "A", size, climateControl, promotions);
         var priceCalculator = new PriceCalculator();
         var price = priceCalculator.CalculatePrice(deposit,
