3c9dea5ce67b40a80156541f85b926fcd09397d5
Manuel <manurz2121@gmail.com>
2024-04-28 20:26:18 -0300

    red: Add TestCanCalculatePriceWithDurationDiscounts

--- BusinessLogic.Test/PriceCalculatorTest.cs
+++ BusinessLogic.Test/PriceCalculatorTest.cs
@@ -21,4 +21,27 @@ public class PriceCalculatorTest
             new Tuple<DateOnly, DateOnly>(_today, _today.AddDays(1)));
         Assert.AreEqual(expectedPrice, price);
     }
+    
+    [TestMethod]
+    [DataRow("Small", false, 7, 332.5)]
+    [DataRow("Small", false, 15, 675)]
+    [DataRow("Medium", false, 7, 498.75)]
+    [DataRow("Medium", false, 15, 1012.5)]
+    [DataRow("Large", false, 7, 665)]
+    [DataRow("Large", false, 15, 1350)]
+    [DataRow("Small", true, 7, 465.5)]
+    [DataRow("Small", true, 15, 945)]
+    [DataRow("Medium", true, 7, 631.75)]
+    [DataRow("Medium", true, 15, 1282.5)]
+    [DataRow("Large", true, 7, 798)]
+    [DataRow("Large", true, 15, 1620)]
+    public void TestCanCalculatePriceWithDurationDiscounts(string size, bool climateControl, int duration,
+        double expectedPrice)
+    {
+        var deposit = new Deposit(1, "A", size, climateControl, new List<Promotion>());
+        var priceCalculator = new PriceCalculator();
+        var price = priceCalculator.CalculatePrice(deposit,
+            new Tuple<DateOnly, DateOnly>(_today, _today.AddDays(duration)));
+        Assert.AreEqual(expectedPrice, price);
+    }
 }
