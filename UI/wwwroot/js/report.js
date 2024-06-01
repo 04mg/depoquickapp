8785633f7a9d86ba4f0031fe3a3a528c6b9c181e
Manuel <manurz2121@gmail.com>
2024-06-01 16:05:10 -0300

    Move JS to report.js

---
+++ UI/wwwroot/js/report.js
@@ -0,0 +1,11 @@
+window.downloadFileFromStream = async (fileName, contentStreamReference) => {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer], { type: 'application/octet-stream' });
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
+}
